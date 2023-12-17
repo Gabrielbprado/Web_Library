fb3f4c08387362d2606e43fd2758534328b503fc
Gabriel Prado <guardavege@gmail.com>
2023-12-17 15:53:14 -0300

    Fixed Bugs

---
+++ src/Models/ErrorViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Web_Library.Models
+{
+    public class ErrorViewModel
+    {
+    }
+}
---
+++ src/wwwroot/js/site.js
@@ -0,0 +1,40 @@
+﻿$(document).ready(function () {
+
+    $('#emprestimos').DataTable(
+        {
+
+            Language:
+            {
+
+                "decimal": "",
+                "emptyTable": "No data available in table",
+                "info": "Mostrando _START_ Um _END_ De _TOTAL_ Entradas",
+                "infoEmpty": "Showing 0 to 0 of 0 entries",
+                "infoFiltered": "(filtered from _MAX_ total entries)",
+                "infoPostFix": "",
+                "thousands": ",",
+                "lengthMenu": "Mostrar _MENU_ Entradas",
+                "loadingRecords": "Carregando",
+                "processing": "",
+                "search": "Pesquisar:",
+                "zeroRecords": "No matching records found",
+                "paginate": {
+                    "first": "Primeiro",
+                    "last": "Ultimo",
+                    "next": "Proximo",
+                    "previous": "Anterior"
+                },
+                "aria": {
+                    "sortAscending": ": activate to sort column ascending",
+                    "sortDescending": ": activate to sort column descending"
+                }
+            }
+
+
+
+        });
+
+    setTimeout(function () {
+        $('.alert').fadeOut('slow');
+    },1000)
+});
