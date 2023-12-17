d47998eb13d8dd6f8ae96a022c566fcf9651c785
Gabriel Prado <guardavege@gmail.com>
2023-12-17 15:39:54 -0300

    Update

---
+++ LivrariaWeb/src/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Web_Library.Models;
+
+namespace Web_Library.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> opts) : base(opts)
+        {
+            
+        }
+        public DbSet<EmprestimosModel> Emprestimos { get; set; }
+    }
+}
